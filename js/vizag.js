ed2ef005c38466e9ee8abddf0a49ac5da3b2e2de
Aman Kosre <kosre.ak29@gmail.com>
2026-06-21 12:41:05 +0530

    Update vizag.js

--- js/vizag.js
+++ js/vizag.js
@@ -138,18 +138,21 @@ window.addEventListener("scroll", () => {
         .style.width = progress + "%";
 });
 var disqus_config = function () {
-                this.page.url = window.location.href;
-                this.page.identifier = window.location.pathname;
-            };
+    this.page.url = window.location.href;
+    this.page.identifier = window.location.pathname;
+};
 
-            (function () {
-                var d = document, s = d.createElement('script');
-                s.src = 'https://wanderwithaman-vercel-app.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-            })();
-            window.addEventListener('scroll', () => {
-                if (window.scrollY > 3000) {
-                    loadDisqus();
-                }
-            }, { once: true });
+(function () {
+    var d = document, s = d.createElement('script');
+    s.src = 'https://wanderwithaman-vercel-app.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 3000) {
+        loadDisqus();
+    }
+}, { once: true });
+<noscript>
+    Please enable JavaScript to view the comments powered by Disqus.
+</noscript>
